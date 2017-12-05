248a34d101f0a6c4b348a4a623793d146bdd7529
SardineFish <Sardine@live.cn>
2017-12-05 23:17:42 +0800

    Add UAV

---
+++ Robot Battle/Assets/UAV/UAV.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UAV : MonoBehaviour {
+    public List<GameObject> Pivots = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
